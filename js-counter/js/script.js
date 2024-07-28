e28c932fa87fdb890ebc871c8c15571f95950f3d
Md.Ashraful Haque <ashrafulhaque@users.noreply.github.com>
2024-07-28 16:38:13 +0600

    JS-Counter: Fixed the local storage feature.Project completed

--- js-counter/js/script.js
+++ js-counter/js/script.js
@@ -1,15 +1,15 @@
 // getItem gets a value from local storage using the key 'storedNumber'
 
-let currentCount = localStorage.getItem('storedNumber') || 0;
-// get the current value of the counter from the dom
-currentCount = document.getElementById("count").innerText;
+let currentCount = localStorage.getItem("storedNumber") || 0;
+// set the current value of the counter from the localstorage or default 0
+document.getElementById("count").innerText = currentCount;
 
 // function to increment the value of counter
 function doIncrement(){
     console.log("doIncrement() function called..");
     let currIncrement = ++currentCount;
     document.getElementById("count").innerText = currIncrement;
-    localStorage.setItem('storedNumber', currIncrement);
+    localStorage.setItem("storedNumber", currIncrement);
 }
 
 // function to decrement the value of counter
@@ -18,5 +18,5 @@ function doDecrement(){
     if (currentCount == 0) return;
     let currDecrement = --currentCount;
     document.getElementById("count").innerText = currDecrement;
-    localStorage.setItem('storedNumber', currDecrement);
+    localStorage.setItem("storedNumber", currDecrement);
 }
